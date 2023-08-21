6524824d3ab0f186b18d71de5432f39056b32766
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-08-21 17:17:51 -0300

    Combinação de shapefiles

--- laboratorios_shape_files.R
+++ laboratorios_shape_files.R
@@ -7,6 +7,9 @@ library(readxl)
 library(geobr)
 library(tidyverse)
 library(viridis)
+library(sf)
+library(spData)
+library(ggrepel)
 
 
 Municipios_da_Amazonia_Legal_2022 <- read_excel("Municipios_da_Amazonia_Legal_2022.xlsx")
@@ -15,12 +18,19 @@ Municipios_da_Amazonia_Legal_2022 <- janitor::clean_names(Municipios_da_Amazonia
 
 mapa_municipios <- readRDS("~/Github/laboratorios_datasicence/mapa_municipios.RDS")
 
+mapa_municipios <- geobr::read_municipality(simplified = FALSE)
+
+estados<- geobr::read_state()
+sedes_municipios<- geobr::read_municipal_seat()
+brasil<- geobr::read_country()
+
 gera_tabela_ibge_municipios<- function(){
   # Load required libraries
   library(httr)
   library(jsonlite)
   library(janitor)
   library(tidyverse)
+  library(colorspace)
   
   # API endpoint URL
   api_url <- "https://apisidra.ibge.gov.br/values/t/4714/n6/all/v/all/p/all/d/v614%202"
@@ -62,6 +72,53 @@ ibge2022<-
   gera_tabela_ibge_municipios()
 
 
+  
+sedes_amazonia<-
+  sedes_municipios %>%
+  inner_join(
+    Municipios_da_Amazonia_Legal_2022 %>%
+      mutate(code_muni = as.numeric(cd_mun))
+  )  
+  
+
+sedes_amazonia$lon<- sf::st_coordinates(sedes_amazonia$geom)[,1]
+sedes_amazonia$lat<- sf::st_coordinates(sedes_amazonia$geom)[,2]  
+  
+
+xmin<- min(sedes_amazonia$lon) -1 
+xmax <- max(sedes_amazonia$lon) +1
+
+ymin<- min(sedes_amazonia$lat) -1
+ymax <- max(sedes_amazonia$lat) +1
+
+
+top_10_populacao<-
+ibge2022 %>%
+  rename(cd_mun = municipio_codigo) %>%
+  inner_join(Municipios_da_Amazonia_Legal_2022) %>%
+  slice_max(order_by = populacao_residente, n=10)
+
+
+sf_top_10_populacao<-
+  sedes_amazonia %>%
+  inner_join(
+    top_10_populacao %>%
+      mutate(code_muni = as.numeric(cd_mun))
+  )
+
+
+data("world")
+
+southamerica<-
+  world %>%
+  filter(continent=="South America" )
+  
+  
+  ggplot() +
+  geom_sf()
+
+
+amazonia_legal_sf<-  
 mapa_municipios %>%
   inner_join(
     Municipios_da_Amazonia_Legal_2022 %>%
@@ -70,10 +127,31 @@ mapa_municipios %>%
   inner_join(
     ibge2022 %>%
       mutate(code_muni = as.numeric(municipio_codigo))
-  ) %>%
-  ggplot() +
-  geom_sf(aes(fill=populacao_residente), color=NA) +
-    scale_fill_viridis()
+  ) 
+
+
+ggplot() +
+  geom_sf(data = southamerica, fill="#DCDCDC")+
+  geom_sf(data= brasil, fill= "#808080")+
+  geom_sf(data=amazonia_legal_sf, aes(fill=populacao_residente/1000), color = "lightgray") +
+  geom_sf(data=estados, fill=NA)+
+  geom_sf(data= sf_top_10_populacao, color="black", size=1)+
+  geom_text_repel(data = sf_top_10_populacao,
+                  aes(x=lon, y=lat, label= str_wrap(paste0(name_muni,":"," ", round(populacao_residente/1000,0)),20)), 
+                  color = "black", 
+                  #fontface = "bold", 
+                  size = 2.9
+  )+
+  coord_sf(xlim = c(xmin,xmax), ylim=c(ymin,ymax))+
+  scale_fill_continuous_sequential(palette= "Heat" )+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= "População em milhares"
+      )
+
     
 
 mapa_municipios %>%
@@ -89,5 +167,6 @@ mapa_municipios %>%
   geom_sf(aes(fill=densidade_demografica), color=NA) +
   scale_fill_viridis()
 
-glimpse(maps::world.cities)
+
+
 
