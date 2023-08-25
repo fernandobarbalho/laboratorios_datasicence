8cfa239fa4365e2adadd5d887a3b82019a9485c7
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-08-25 18:25:02 -0300

    REfatoração do código

--- laboratorio_shapefiles_america_sul.R
+++ laboratorio_shapefiles_america_sul.R
@@ -82,8 +82,7 @@ southamerica<-
 southamerica$lon<- sf::st_coordinates(sf::st_centroid(southamerica$geom))[,1]   
 southamerica$lat<- sf::st_coordinates(sf::st_centroid(southamerica$geom))[,2]
 
-world %>%
-  filter(continent == "South America") %>%
+southamerica %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
   scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
@@ -94,29 +93,23 @@ world %>%
   labs(
     fill= str_wrap("População", 30)
   )+
-  geom_text_repel(data = southamerica,
-            aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
-            color = "black", 
-            fontface = "bold", 
-            size = 2.9)
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)
 
 
 
-sf::st_centroid(franca[[11]][[1]])
+
 
 #Mapa da França
-franca<-
+france<-
   world %>%
   filter(iso_a2 == "FR")
   
 
-brasil_spdata<-
-  world %>%
-  filter(iso_a2 == "BR")
 
-
-world %>%
-  filter(iso_a2 == "FR") %>%
+france %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
   scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
@@ -132,20 +125,61 @@ world %>%
 
 #Mapa da América do Sul incluindo a França
 
+southamerica %>%
+  bind_rows(france) %>%
+  ggplot() +
+  geom_sf(aes(fill=pop)) +
+  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  theme_light() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= str_wrap("População", 30)
+  )+
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)
 
-fab<-
-insee::get_dataset_list()
-
-fab2<-
-  insee::get_insee_idbank("TCRED-ESTIMATIONS-POPULATION")
 
 
-idbank_list <- insee::get_idbank_list('TCRED-ESTIMATIONS-POPULATION')
+#Mapa recortado da América do Sul incluindo a França
+
+
+
+southamerica %>%
+  bind_rows(france) %>%
+  mutate(lon= ifelse(iso_a2=="FR", france[[11]][[1]][[1]][[1]][1,1], lon),
+         lat= ifelse(iso_a2=="FR",france[[11]][[1]][[1]][[1]][1,2], lat)) %>%
+  ggplot() +
+  geom_sf(aes(fill=pop)) +
+  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)+
+  coord_sf(xlim = c(-82,-35), ylim=c(-60,15))+
+  theme_light() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= str_wrap("População", 30)
+  )
+
+
+
+
+#Acrescenta os dados de população da Guiana Francesa
 
 data_guiana<-
-  insee::get_insee_idbank("001760178")
-
-
+  insee::get_idbank_list('TCRED-ESTIMATIONS-POPULATION') %>%
+  filter(str_detect(REF_AREA_label_fr,"Guyane")) %>%
+  filter(AGE == "00-") %>% #all ages
+  filter(SEXE == 0) %>% #men and women
+  pull(idbank) %>%
+  insee::get_insee_idbank()
 
 
 ggplot() +
