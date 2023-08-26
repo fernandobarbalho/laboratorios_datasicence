57534d24a56ff0c5b2c5def065eeb4b32b6e3c26
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-08-26 14:02:00 -0300

    Detalhamento do mapa do Brasil por estado

--- laboratorio_shapefiles_america_sul.R
+++ laboratorio_shapefiles_america_sul.R
@@ -6,6 +6,48 @@ library(spData)
 library(ggrepel)
 
 
+get_municipalies_data<- function(){
+  # Load required libraries
+  library(httr)
+  library(jsonlite)
+  library(janitor)
+  library(tidyverse)
+  library(colorspace)
+  
+  # API endpoint URL
+  api_url <- "https://apisidra.ibge.gov.br/values/t/4714/n6/all/v/all/p/all/d/v614%202"
+  
+  data_list <- fromJSON(api_url)
+  
+  
+  names_df<- data_list[1,]
+  data_list <- data_list[-1,]
+  
+  names(data_list) <- names_df
+  
+  data_list <- janitor::clean_names(data_list)
+  
+  ibge_municipios<-
+    data_list %>%
+    mutate(valor =as.numeric(valor)) %>%
+    select(municipio_codigo,
+           municipio,
+           variavel,
+           valor,
+           ano) %>%
+    pivot_wider(id_cols = c(municipio_codigo, municipio), names_from = variavel, values_from = valor) %>%
+    separate(municipio, into = c("municipio", "uf"), sep = " - ")
+  
+  
+  ibge_municipios<- janitor::clean_names(ibge_municipios)
+  
+  ibge_municipios
+  
+  
+}
+
+
+
 data("world")
 
 
@@ -16,7 +58,7 @@ data("world")
 world %>%
   ggplot() +
   geom_sf(aes(fill=pop/10^6)) +
-  scale_fill_continuous_sequential(palette= "Heat" )+
+  scale_fill_continuous_sequential(palette= "Heat 2" )+
   theme_void() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -29,7 +71,7 @@ world %>%
 world %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   theme_void() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -46,7 +88,7 @@ world %>%
   filter(continent == "South America") %>%
   ggplot() +
   geom_sf(aes(fill=pop/10^6)) +
-  scale_fill_continuous_sequential(palette= "Heat" )+
+  scale_fill_continuous_sequential(palette= "Heat 2" )+
   theme_void() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -61,7 +103,7 @@ world %>%
   filter(continent == "South America") %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   theme_void() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -85,7 +127,7 @@ southamerica$lat<- sf::st_coordinates(sf::st_centroid(southamerica$geom))[,2]
 southamerica %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   theme_light() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -112,7 +154,7 @@ france<-
 france %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   theme_light() +
   theme(
     #panel.background = element_rect(fill="#0077be")
@@ -129,7 +171,7 @@ southamerica %>%
   bind_rows(france) %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   theme_light() +
   theme(
     panel.background = element_rect(fill="#0077be")
@@ -154,7 +196,7 @@ southamerica %>%
          lat= ifelse(iso_a2=="FR",france[[11]][[1]][[1]][[1]][1,2], lat)) %>%
   ggplot() +
   geom_sf(aes(fill=pop)) +
-  scale_fill_continuous_sequential(palette= "Heat", trans= "log2" )+
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
   geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
                   color = "black", 
                   fontface = "bold", 
@@ -179,7 +221,112 @@ data_guiana<-
   filter(AGE == "00-") %>% #all ages
   filter(SEXE == 0) %>% #men and women
   pull(idbank) %>%
-  insee::get_insee_idbank()
+  insee::get_insee_idbank() %>%
+  filter(TIME_PERIOD == "2023") %>% 
+  select(TITLE_EN,OBS_VALUE) %>%
+  mutate(iso_a2 = "FR")
+
+data_guiana <- janitor::clean_names(data_guiana)
+
+southamerica %>%
+  bind_rows(france) %>%
+  left_join(data_guiana) %>%
+  mutate(pop=ifelse(iso_a2=="FR",obs_value,pop))%>%
+  mutate(lon= ifelse(iso_a2=="FR", france[[11]][[1]][[1]][[1]][1,1], lon),
+         lat= ifelse(iso_a2=="FR",france[[11]][[1]][[1]][[1]][1,2], lat)) %>%
+  ggplot() +
+  geom_sf(aes(fill=pop)) +
+  scale_fill_continuous_sequential(palette= "Heat 2", trans= "log2" )+
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)+
+  coord_sf(xlim = c(-82,-35), ylim=c(-60,15))+
+  theme_light() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= str_wrap("População", 30)
+  )
+
+
+#incluindo a América Central
+
+central_america<-
+  world %>%
+  filter(subregion == "Central America")
+
+
+southamerica %>%
+  bind_rows(france) %>%
+  left_join(data_guiana) %>%
+  mutate(pop=ifelse(iso_a2=="FR",obs_value,pop))%>%
+  mutate(lon= ifelse(iso_a2=="FR", france[[11]][[1]][[1]][[1]][1,1], lon),
+         lat= ifelse(iso_a2=="FR",france[[11]][[1]][[1]][[1]][1,2], lat)) %>%
+  ggplot() +
+  geom_sf(aes(fill=pop/10^6)) +
+  geom_sf(data= central_america,fill= "#808080")+
+  scale_fill_continuous_sequential(palette= "Heat 2" )+
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)+
+  coord_sf(xlim = c(-82,-35), ylim=c(-60,15))+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= str_wrap("População em milhões", 10)
+  )
+
+
+##Mapa com população dos estados do Brasil
+
+brasil<- geobr::read_country()
+estados<- geobr::read_state()
+
+#dados de população
+
+ibge2022<-
+  get_municipalies_data()
+
+estados<-
+  estados %>%
+  inner_join(
+    ibge2022 %>%
+      rename(abbrev_state = uf) %>%
+      summarise(.by=abbrev_state,
+                pop = sum(populacao_residente)
+      )
+  )
+
+southamerica %>%
+  filter(iso_a2!="BR") %>%
+  bind_rows(france) %>%
+  left_join(data_guiana) %>%
+  mutate(pop=ifelse(iso_a2=="FR",obs_value,pop))%>%
+  mutate(lon= ifelse(iso_a2=="FR", france[[11]][[1]][[1]][[1]][1,1], lon),
+         lat= ifelse(iso_a2=="FR",france[[11]][[1]][[1]][[1]][1,2], lat)) %>%
+  ggplot() +
+  geom_sf(aes(fill=pop/10^6)) +
+  geom_sf(data=estados, aes(fill=pop/10^6)) +
+  geom_sf(data=brasil,fill=NA, color="green")+
+  geom_sf(data= central_america,fill= "#808080")+
+  scale_fill_continuous_sequential(palette= "Heat 2" )+
+  geom_text_repel(aes(x=lon, y=lat, label= str_wrap(name_long,20)), 
+                  color = "black", 
+                  fontface = "bold", 
+                  size = 2.9)+
+  coord_sf(xlim = c(-82,-35), ylim=c(-60,15))+
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill="#0077be")
+  ) +
+  labs(
+    fill= str_wrap("População em milhões", 10)
+  )
 
 
 ggplot() +
@@ -205,7 +352,7 @@ ggplot() +
   )+
   
   coord_sf(xlim = c(xmin,xmax), ylim=c(ymin,ymax))+
-  scale_fill_continuous_sequential(palette= "Heat" )+
+  scale_fill_continuous_sequential(palette= "Heat 2" )+
   theme_void() +
   theme(
     panel.background = element_rect(fill="#0077be")
